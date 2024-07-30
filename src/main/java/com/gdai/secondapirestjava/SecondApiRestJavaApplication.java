30ef2ae5082f4897db5679ae2b6995fa6559108d
mandi <armancanjer@gmail.com>
2024-07-30 17:43:49 -0400

    feat(): add first maven init project

---
+++ src/main/java/com/gdai/secondapirestjava/SecondApiRestJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gdai.secondapirestjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondApiRestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondApiRestJavaApplication.class, args);
+	}
+
+}
