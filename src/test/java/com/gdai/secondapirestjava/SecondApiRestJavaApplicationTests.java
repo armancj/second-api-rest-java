c34bcb5ddd4651a98752fc6c02ee5455eebb8a8f
mandi <armancanjer@gmail.com>
2024-07-30 17:21:02 -0400

    feat(): add new project init java

---
+++ src/test/java/com/gdai/secondapirestjava/SecondApiRestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdai.secondapirestjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApiRestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
